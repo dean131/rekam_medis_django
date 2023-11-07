e39709774c39e47d0081dc5371dd8faa672314bc
dean131 <deanabdillah@gmail.com>
2023-11-07 22:18:06 +0700

    - add is_full to dokter tersedia

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -22,12 +22,25 @@ class PemeriksaanModelSerializer(serializers.ModelSerializer):
 class JadwalDokterModelSerializer(serializers.ModelSerializer):
     dokter_id = serializers.SerializerMethodField('get_dokter_id')
     nama_dokter = serializers.SerializerMethodField('get_nama_dokter')
+    foto = serializers.SerializerMethodField('get_foto')
+    is_full = serializers.SerializerMethodField('get_is_full')
+
     class Meta:
         model = JadwalDokter
-        fields = ['id', 'dokter_id', 'nama_dokter', 'hari', 'jam_mulai', 'jam_selesai']
+        fields = ['id', 'dokter_id', 'nama_dokter', 'jam_mulai', 'jam_selesai', 'is_full', 'foto']
 
     def get_dokter_id(self, obj):
         return obj.dokter.id
     
     def get_nama_dokter(self, obj):
         return obj.dokter.user.nama_lengkap
+    
+    def get_foto(self, obj):
+        if obj.dokter.user.foto:
+            return obj.dokter.user.foto.url
+        return None
+    
+    def get_is_full(self, obj):
+        if self.context['count'] >= obj.dokter.max_pasien:
+            return True
+        return False
